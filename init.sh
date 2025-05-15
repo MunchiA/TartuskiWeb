fc94a12e7ce4a994284335a8a1c97b7c5710ae37
Isaacva8 <isaacva8@gmail.com>
2025-05-15 17:09:35 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,10 +10,6 @@ fi
 sudo dnf update -y
 sudo dnf install -y python3 python3-pip python3-devel gcc openssl-devel libffi-devel mariadb105-devel git make pkgconf zlib-devel bzip2-devel readline-devel sqlite-devel tk-devel libuuid-devel xz-devel mariadb105
 
-# Clonar el proyecto
-git clone https://github.com/MunchiA/TartuskiWeb.git
-cd TartuskiWeb
-
 # Crear y activar entorno virtual
 python3 -m venv venv
 source venv/bin/activate
